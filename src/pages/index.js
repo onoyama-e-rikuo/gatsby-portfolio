7ee6a6c8b34852258ab7d8abaf0e5a62b3df2d67
onoyama-e-rikuo <evang00dd@gmail.com>
2020-05-21 12:07:41 +0900

    change config

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,6 @@ import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Hero from "../components/sections/hero"
-import Articles from "../components/sections/articles" 
 import About from "../components/sections/about"
 import Interests from "../components/sections/interests"
 import Projects from "../components/sections/projects"
@@ -13,10 +12,8 @@ import Contact from "../components/sections/contact"
 
 const IndexPage = ({ data }) => (
   <Layout splashScreen={true}>
-    <SEO title="Portfolio Minimal - A Gatsby Starter." />
+    <SEO title="oEr. | Portfolio Site" />
     <Hero content={data.hero.edges} />
-    {/* Articles is populated via Medium RSS Feed fetch */}
-    <Articles />
     <About content={data.about.edges} />
     <Interests content={data.interests.edges} />
     <Projects content={data.projects.edges} />
